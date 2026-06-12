861cb85497548b40bcdf8ec48d8a51f580d033ef
ljs <ljs85633@gmail.com>
2026-06-12 18:15:33 +0900

    add console

---
+++ StartConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using Sequence;
+
+namespace StartConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var hs = new Hotspot();
+            var data = hs.Run("", "", 71).Result;
+
+            Console.WriteLine("connected: " + data);
+        }
+    }
+}
